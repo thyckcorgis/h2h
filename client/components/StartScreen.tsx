602b4959909d2d1e438242de62201301695720e3
Annette Lau <annette1@ualberta.ca>
2021-01-16 14:31:41 -0700

    made start screen

--- client/components/StartScreen.tsx
+++ client/components/StartScreen.tsx
@@ -1,6 +1,6 @@
 import { StackNavigationHelpers } from "@react-navigation/stack/lib/typescript/src/types";
 import * as React from "react";
-import { View, Text, Button } from "react-native";
+import { View, Text, Button, Image, StyleSheet } from "react-native";
 
 interface StartScreenProps {
   navigation: StackNavigationHelpers;
@@ -8,9 +8,38 @@ interface StartScreenProps {
 
 export default function LoadingScreen({ navigation }: StartScreenProps) {
   return (
-    <View>
+    <View style={styles.screen}>
+      <Image source={require("../assets/images/h2h_logo.png")} />
       <Text>This is the start screen....</Text>
       <Button title="next" onPress={() => navigation.navigate("Register")} />
+      <Image source={require("../assets/images/corgi_logo.png")} />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  screen: {
+    padding: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    flex: 1,
+    backgroundColor: "black",
+  },
+  bigText: {
+    fontFamily: "Georgia",
+    fontSize: 30,
+    color: "white",
+  },
+  smallText: {
+    fontFamily: "Georgia",
+    fontSize: 18,
+    color: "white",
+    textAlign: "center",
+  },
+  continueText: {
+    fontFamily: "Comfortaa",
+    fontSize: 14,
+    color: "#892cdc",
+    paddingVertical: 30,
+  },
+});
