308fc8616b09cebe335e2a05e10b9c0350d9df2d
Annette Lau <annette1@ualberta.ca>
2021-01-16 14:45:41 -0700

    styled the home scren it so ugleeeyyyy

--- client/components/HomeScreen.tsx
+++ client/components/HomeScreen.tsx
@@ -1,6 +1,6 @@
 import { StackNavigationHelpers } from "@react-navigation/stack/lib/typescript/src/types";
 import * as React from "react";
-import { View, Text, Button } from "react-native";
+import { View, Text, Button, StyleSheet } from "react-native";
 import { TextInput } from "react-native-gesture-handler";
 import { useState } from "react";
 import { io } from "socket.io-client";
@@ -30,17 +30,55 @@ export default function HomeScreen({ navigation, route }: HomeScreenProps) {
   };
 
   return (
-    <View>
-      <Text>Host Confessation</Text>
+    <View style={styles.screen}>
+      <Text style={styles.bigText}>Host Confessation</Text>
       <Button title="Host" onPress={hostGameHandler} />
-      <Text>Join Confessation</Text>
+      <Text style={styles.bigText}>Join Confessation</Text>
       <TextInput
         placeholder="Enter a room code"
         onChangeText={(text) => setCode(text)}
         value={code}
         keyboardType="number-pad"
+        style={styles.inputField}
       />
       <Button title="Join" onPress={joinGameHandler} />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  screen: {
+    padding: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    flex: 1,
+    backgroundColor: "black",
+  },
+  bigText: {
+    fontSize: 30,
+    color: "white",
+  },
+  inputField: {
+    padding: 5,
+    margin: 10,
+    width: 250,
+    textAlign: "left",
+    alignSelf: "center",
+    borderBottomWidth: 1,
+    borderColor: "white",
+    fontSize: 18,
+    color: "white",
+  },
+  smallText: {
+    fontFamily: "Georgia",
+    fontSize: 18,
+    color: "white",
+    textAlign: "center",
+  },
+  continueText: {
+    fontFamily: "Comfortaa",
+    fontSize: 14,
+    color: "#892cdc",
+    paddingVertical: 30,
+  },
+});
--- client/components/StartScreen.tsx
+++ client/components/StartScreen.tsx
@@ -1,6 +1,7 @@
 import { StackNavigationHelpers } from "@react-navigation/stack/lib/typescript/src/types";
 import * as React from "react";
 import { View, Text, Button, Image, StyleSheet } from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
 
 interface StartScreenProps {
   navigation: StackNavigationHelpers;
@@ -9,10 +10,13 @@ interface StartScreenProps {
 export default function LoadingScreen({ navigation }: StartScreenProps) {
   return (
     <View style={styles.screen}>
-      <Image source={require("../assets/images/h2h_logo.png")} />
-      <Text>This is the start screen....</Text>
-      <Button title="next" onPress={() => navigation.navigate("Register")} />
-      <Image source={require("../assets/images/corgi_logo.png")} />
+      <TouchableOpacity onPress={() => navigation.navigate("Register")}>
+        <Image source={require("../assets/images/h2h_logo.png")} />
+        <Text>This is the start screen....</Text>
+        <View style={styles.corgiBox}>
+          <Image source={require("../assets/images/corgi_logo.png")} />
+        </View>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -41,5 +45,8 @@ const styles = StyleSheet.create({
     fontSize: 14,
     color: "#892cdc",
     paddingVertical: 30,
+  },
+  corgiBox: {
+    alignItems: "center",
   },
 });
