3de01b267ef12fa29709b2f476055d8247893613
Charles Ancheta <cancheta@ualberta.ca>
2021-01-16 14:23:25 -0700

    add hostGame handler

--- client/components/HomeScreen.tsx
+++ client/components/HomeScreen.tsx
@@ -2,27 +2,37 @@ import { StackNavigationHelpers } from "@react-navigation/stack/lib/typescript/s
 import * as React from "react";
 import { View, Text, Button } from "react-native";
 import { TextInput } from "react-native-gesture-handler";
-import {useState} from 'react';
-
+import { useState } from "react";
+import { io } from "socket.io-client";
 
 interface HomeScreenProps {
   navigation: StackNavigationHelpers;
+  route: any;
 }
 
-export default function HomeScreen({ navigation }: HomeScreenProps) {
-    const [code, setCode] = useState("");
+export default function HomeScreen({ navigation, route }: HomeScreenProps) {
+  const [code, setCode] = useState("");
+  const { name } = route.params;
 
+  const hostGameHandler = () => {
+    const socket = io("http://thyck.top", {
+      path: "/h2h",
+    });
+    socket.emit("create-game", name, (code: number) => {
+      navigation.navigate("Waiting", { code });
+    });
+  };
 
   return (
     <View>
       <Text>Host Confessation</Text>
-      <Button title="Host" onPress={() => navigation.navigate("Waiting")} />
+      <Button title="Host" onPress={hostGameHandler} />
       <Text>Join Confessation</Text>
-      <TextInput 
+      <TextInput
         placeholder="Enter a room code"
         onChangeText={(text) => setCode(text)}
         value={code}
-        keyboardType='number-pad'
+        keyboardType="number-pad"
       />
       <Button title="Join" onPress={() => navigation.navigate("Waiting")} />
     </View>
