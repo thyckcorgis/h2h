d4eb12be6ac8a16245f8bf893ba654bae9627568
Charles Ancheta <cancheta@ualberta.ca>
2021-01-16 16:47:33 -0700

    these brackets to be lookin kinda slim doe

--- server/index.js
+++ server/index.js
@@ -26,10 +26,8 @@ const drawCard = (code) => {
 const shuffleArray = (array) => {
   const arr = [...array];
   for (let i = arr.length - 1; i > 0; --i) {
-    const j = (Math.floor(Math.random() * (i + 1))[(arr[i], arr[j])] = [
-      arr[j],
-      arr[i],
-    ]);
+    const j = Math.floor(Math.random() * (i + 1));
+    [arr[i], arr[j]] = [arr[j], arr[i]];
   }
   return arr;
 };
