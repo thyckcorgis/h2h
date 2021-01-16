a4f4f0a4bf3be8c448663614af322a09a73256bb
Annette Lau <annette1@ualberta.ca>
2021-01-16 16:39:08 -0700

    make it FIT ITS TOO THYCKKKK

--- client/components/HomeScreen.tsx
+++ client/components/HomeScreen.tsx
@@ -1,6 +1,15 @@
 import { StackNavigationHelpers } from "@react-navigation/stack/lib/typescript/src/types";
 import * as React from "react";
-import { View, Text, Button, StyleSheet, TextInput, Image, TouchableWithoutFeedback, Keyboard } from "react-native";
+import {
+  View,
+  Text,
+  Button,
+  StyleSheet,
+  TextInput,
+  Image,
+  TouchableWithoutFeedback,
+  Keyboard,
+} from "react-native";
 import { useState } from "react";
 import { TouchableOpacity } from "react-native-gesture-handler";
 
@@ -14,7 +23,6 @@ interface HomeScreenProps {
 export default function HomeScreen({ navigation, route }: HomeScreenProps) {
   const [code, setCode] = useState("");
   const { name } = route.params;
-
 
   const hostGameHandler = () => {
     socket.emit("create", name, (code: number) => {
@@ -30,46 +38,39 @@ export default function HomeScreen({ navigation, route }: HomeScreenProps) {
     });
   };
 
-  const DismissKeyboard = ({children} : {children:any}) => (
-    <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
-      {children}
-    </TouchableWithoutFeedback>
-  );
-
   return (
-    <DismissKeyboard>
     <View style={styles.screen}>
-        <View style={styles.container}>
-            <Text style={styles.bigText}>Host Confessation</Text>
-            <TouchableOpacity onPress={hostGameHandler}>
-            <Image source={require("../assets/images/host_button.png")} />
-            </TouchableOpacity>
-        </View>
-        <View style={styles.container}>
-            <Text style={styles.bigText}>Join Confessation</Text>
-            <TextInput
-                placeholder="Enter a room code"
-                placeholderTextColor='white'
-                onChangeText={(text) => setCode(text)}
-                value={code}
-                keyboardType="number-pad"
-                style={styles.inputField}
-            />
-            <TouchableOpacity onPress={joinGameHandler}>
-            <Image source={require("../assets/images/join_button.png")} />
-            </TouchableOpacity>
-        </View>
+      <View style={{ flex: 1 }} />
+      <View style={styles.container}>
+        <Text style={styles.bigText}>Host Confessation</Text>
+        <TouchableOpacity onPress={hostGameHandler}>
+          <Image source={require("../assets/images/host_button.png")} />
+        </TouchableOpacity>
+      </View>
+      <View style={styles.container}>
+        <Text style={styles.bigText}>Join Confessation</Text>
+        <TextInput
+          placeholder="Enter a room code"
+          placeholderTextColor="white"
+          onChangeText={(text) => setCode(text)}
+          value={code}
+          keyboardType="number-pad"
+          style={styles.inputField}
+        />
+        <TouchableOpacity onPress={joinGameHandler}>
+          <Image source={require("../assets/images/join_button.png")} />
+        </TouchableOpacity>
+      </View>
+      <View style={{ flex: 2 }} />
     </View>
-    </DismissKeyboard>
   );
 }
 
 const styles = StyleSheet.create({
   screen: {
     padding: 50,
-    paddingTop: 200,
     alignItems: "center",
-    // justifyContent: "center",
+    justifyContent: "center",
     flex: 1,
     backgroundColor: "black",
   },
