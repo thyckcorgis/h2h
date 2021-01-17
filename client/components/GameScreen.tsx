44af7e2449a972588c1f14ebcd759723e2f1c434
Charles Ancheta <cancheta@ualberta.ca>
2021-01-16 17:58:50 -0700

    oop

--- client/components/GameScreen.tsx
+++ client/components/GameScreen.tsx
@@ -39,8 +39,9 @@ export default function GameScren({ route, navigation }: GameScreenProps) {
 
   const [modalVisible, setModalVisible] = useState(false);
 
-  let button = isTurn(name, currentPlayer) ? <Button title="next" onPress={nextCardHandler} /> : '';
-
+  const button = isTurn(name, currentPlayer) ? (
+    <Button title="next" onPress={nextCardHandler} />
+  ) : null;
 
   return (
     <View style={styles.screen}>
@@ -62,7 +63,7 @@ export default function GameScren({ route, navigation }: GameScreenProps) {
       >
         <Text style={styles.smallText}>Participants</Text>
       </TouchableHighlight>
-      <View>{button}</View>
+      {button}
     </View>
   );
 }
