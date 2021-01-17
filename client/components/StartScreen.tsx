f76ab458aff399d8eea5189898b7a11ae4de9e51
Charles Ancheta <cancheta@ualberta.ca>
2021-01-16 22:24:54 -0700

    idk which config file is actually making it work but i'll put in all of them

--- client/components/StartScreen.tsx
+++ client/components/StartScreen.tsx
@@ -1,7 +1,13 @@
 import React from "react";
-import { View, Text, Image, StyleSheet } from "react-native";
+import { View, Text, Image, StyleSheet, Dimensions } from "react-native";
 import { TouchableOpacity } from "react-native-gesture-handler";
 import { StackNavigationHelpers } from "@react-navigation/stack/lib/typescript/src/types";
+
+import H2HLogo from "../assets/images/h2h_logo.svg";
+import CorgiLogo from "../assets/images/corgi_logo.svg";
+
+const screenWidth = Dimensions.get("window").width;
+const screenHeight = Dimensions.get("window").height;
 
 interface StartScreenProps {
   navigation: StackNavigationHelpers;
@@ -12,13 +18,14 @@ export default function LoadingScreen({ navigation }: StartScreenProps) {
     <View style={styles.screen}>
       <TouchableOpacity onPress={() => navigation.navigate("Register")}>
         <View style={styles.screen}>
-          <Image source={require("../assets/images/h2h_logo.png")} />
+          <H2HLogo width={screenWidth} />
           <Text style={styles.continueText}>Tap to start.</Text>
         </View>
       </TouchableOpacity>
 
       <View style={styles.corgiBox}>
-        <Image source={require("../assets/images/corgi_logo.png")} />
+        {/* <Image source={require("../assets/images/corgi_logo.png")} /> */}
+        <CorgiLogo width={screenWidth} />
       </View>
     </View>
   );
