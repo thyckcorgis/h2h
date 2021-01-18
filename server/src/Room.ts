32fa95ae8f69f80eedcfa53cd90ed49e895f2634
Charles Ancheta <cancheta@ualberta.ca>
2021-01-17 23:56:41 -0700

    complete Room class

--- server/src/Room.ts
+++ server/src/Room.ts
@@ -1,3 +1,14 @@
+import categories from "./categories.json";
+import { shuffleArray } from "./helpers";
+
+interface Settings {
+  happy: boolean;
+  heavy: boolean;
+  toTheSpeaker: boolean;
+  selfReflection: boolean;
+  customCards: boolean;
+}
+
 export default class Room {
   users: string[];
   currentPlayerIdx: number;
@@ -5,16 +16,89 @@ export default class Room {
   customCards: string[];
   gameStarted: boolean;
   currentCard: string;
-  settings: {
-    happy: boolean;
-    heavy: boolean;
-    toTheSpeaker: boolean;
-    selfReflection: boolean;
-    customCards: boolean;
-  };
-
+  settings: Settings;
   constructor(hostName: string) {
     this.users = [hostName];
     this.currentPlayerIdx = 0;
+    this.cards = [];
+    this.customCards = [];
+    this.gameStarted = false;
+    this.currentCard = "";
+    this.settings = {
+      happy: true,
+      heavy: true,
+      toTheSpeaker: true,
+      selfReflection: true,
+      customCards: false,
+    };
+  }
+
+  endTurn() {
+    this.currentPlayerIdx = (this.currentPlayerIdx + 1) % this.users.length;
+  }
+
+  setSettings(settings: Settings) {
+    this.settings = settings;
+  }
+
+  getCurrentPlayer() {
+    return this.users[this.currentPlayerIdx];
+  }
+
+  drawCard() {
+    const card = this.cards.pop();
+    this.currentCard = card || "";
+    return card;
+  }
+
+  getCurrentCard() {
+    return this.currentCard;
+  }
+
+  getUsers() {
+    return [...this.users];
+  }
+
+  addUser(userName: string) {
+    this.users.push(userName);
+  }
+
+  removeUser(name: string) {
+    this.users = this.users.filter((user) => user !== name);
+    this.currentPlayerIdx = this.currentPlayerIdx % this.users.length;
+  }
+
+  userExists(name: string) {
+    return this.users.includes(name);
+  }
+
+  getNewHost(isHost: boolean) {
+    return isHost ? this.users[0] : "";
+  }
+
+  startGame() {
+    this.gameStarted = true;
+  }
+
+  customCardsEnabled() {
+    return this.settings.customCards;
+  }
+
+  addCustomCard(question: string) {
+    this.customCards.push(question);
+  }
+
+  createCardDeck() {
+    Object.entries(this.settings).forEach(([key, val]) => {
+      if (val && key !== "customCards")
+        this.cards = this.cards.concat(categories[key]);
+    });
+
+    if (this.settings.customCards) {
+      this.cards = this.cards.concat(this.customCards);
+      this.customCards = [];
+    }
+
+    this.cards = shuffleArray(this.cards);
   }
 }
