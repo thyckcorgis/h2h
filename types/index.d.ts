c2b1ee97f90d701ff77116ad00ccccde7d00728e
Charles Ancheta <cancheta@ualberta.ca>
2021-01-22 23:16:51 -0700

    all the exported types go here now

---
+++ types/index.d.ts
@@ -0,0 +1,18 @@
+export {
+  AnonEventHandler,
+  EventHandler,
+  QuitHandler,
+  SettingHandler,
+  SocketEvent,
+} from "./handlers";
+
+export {
+  ServerMessageResponse,
+  NextCardResponse,
+  JoinServerResponse,
+  StartGameResponse,
+  QuitLobbyResponse,
+  QuitGameResponse,
+} from "./responses";
+
+export { default as Settings } from "./settings";
