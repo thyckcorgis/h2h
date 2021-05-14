b19ff9008e9e1b02c62c2041904a2e2ff617c87e
Annette Lau <annette1@ualberta.ca>
2021-05-14 00:56:09 -0600

    cleaned loading screen (global styles, safeview)

--- client-mobile/components/screens/LoadingScreen.tsx
+++ client-mobile/components/screens/LoadingScreen.tsx
@@ -1,39 +1,33 @@
 import React from "react";
 import { StyleSheet, View, Text, TouchableOpacity } from "react-native";
 import Styles from "../styles";
+import SafeView from "../basics/SafeView";
 import ScreenProps from "../ScreenProps";
 
 export default function LoadingScreen({ navigation }: ScreenProps) {
   return (
-    <View style={Styles.screen}>
+    <SafeView>
       <TouchableOpacity
         onPress={() => navigation.navigate("Start")}
         style={Styles.screen}
       >
         <Text style={Styles.bigText}>confessation</Text>
-        <View style={{ flexDirection: "row", paddingVertical: 30 }}>
-          <Text style={{ ...Styles.smallText, fontStyle: "italic" }}>
-            [ˌkɑn.fɛsˈseɪ.ʃən]
-          </Text>
-          <Text style={{ ...Styles.smallText, fontWeight: "bold" }}>
-            {" "}
-            ⋅ noun
-          </Text>
+        <View style={styles.rowAlign}>
+          <Text style={styles.italicsText}>[ˌkɑn.fɛsˈseɪ.ʃən]</Text>
+          <Text style={styles.boldText}> ⋅ noun</Text>
         </View>
         <Text style={Styles.smallText}>
           a difficult but meaningful conversation.
         </Text>
         <Text style={styles.continueText}>Tap to continue.</Text>
       </TouchableOpacity>
-    </View>
+    </SafeView>
   );
 }
 
 const styles = StyleSheet.create({
-  continueText: {
-    fontFamily: "Avenir-Light",
-    fontSize: 14,
-    color: "#892cdc",
-    paddingVertical: "10%",
-  },
+  continueText: { ...Styles.continueText, paddingVertical: "10%" },
+  boldText: { ...Styles.smallText, fontWeight: "bold" },
+  italicsText: { ...Styles.smallText, fontStyle: "italic" },
+  rowAlign: { flexDirection: "row", paddingVertical: "10%" },
 });
