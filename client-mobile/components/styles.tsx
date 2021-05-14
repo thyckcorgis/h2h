7eac8b6c2119f9513c8c031f471c3fc5de412e21
Annette Lau <annette1@ualberta.ca>
2021-05-14 00:23:24 -0600

    made global stylesheet

---
+++ client-mobile/components/styles.tsx
@@ -0,0 +1,22 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+  screen: {
+    padding: "10%",
+    alignItems: "center",
+    justifyContent: "center",
+    flex: 1,
+    backgroundColor: "black",
+  },
+  bigText: {
+    fontFamily: "Georgia",
+    fontSize: 30,
+    color: "white",
+  },
+  smallText: {
+    fontFamily: "Georgia",
+    fontSize: 18,
+    color: "white",
+    textAlign: "center",
+  },
+});
