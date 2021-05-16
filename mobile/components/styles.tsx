ad54fcb24d4154cf75f7949b1b1f111eb0e879b2
Charles Ancheta <55412395+cbebe@users.noreply.github.com>
2021-05-15 21:02:38 -0600

    inherit from `baseText`

--- mobile/components/styles.tsx
+++ mobile/components/styles.tsx
@@ -1,5 +1,8 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet, TextStyle } from "react-native";
 
+const baseText = { fontFamily: "Avenir-Light", color: "white" };
+const smallText: TextStyle = { ...baseText, fontSize: 18, textAlign: "center" };
+const continueText = { ...baseText, fontSize: 14, color: "#892cdc" };
 export default StyleSheet.create({
   screen: {
     padding: "10%",
@@ -7,29 +10,19 @@ export default StyleSheet.create({
     justifyContent: "center",
     flex: 1,
     backgroundColor: "black",
-    // borderColor: "red",
-    // borderWidth: 1,
   },
-  bigText: { fontFamily: "Avenir-Light", fontSize: 30, color: "white" },
-  smallText: { fontFamily: "Avenir-Light", fontSize: 18, color: "white", textAlign: "center" },
-  continueText: { fontFamily: "Avenir-Light", fontSize: 14, color: "#892cdc" },
-  errorText: {
-    fontFamily: "Avenir-Light",
-    fontSize: 14,
-    textAlign: "center",
-    color: "red",
-    padding: 10,
-  },
+  bigText: { ...baseText, fontSize: 30 },
+  smallText,
+  continueText,
+  errorText: { ...continueText, textAlign: "center", color: "red", padding: 10 },
   inputField: {
+    ...smallText,
+    textAlign: "left",
     padding: "2%",
     paddingTop: "5%",
     width: "75%",
-    textAlign: "left",
     alignSelf: "center",
     borderBottomWidth: 1,
     borderColor: "white",
-    fontSize: 18,
-    color: "white",
-    fontFamily: "Avenir-Light",
   },
 });
