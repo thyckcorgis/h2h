b3eaa240081081d5f195ef3d841a85f60ce02039
Charles Ancheta <55412395+cbebe@users.noreply.github.com>
2021-05-15 21:04:37 -0600

    add `baseText` to `StyleSheet`

--- mobile/components/styles.tsx
+++ mobile/components/styles.tsx
@@ -4,6 +4,9 @@ const baseText = { fontFamily: "Avenir-Light", color: "white" };
 const smallText: TextStyle = { ...baseText, fontSize: 18, textAlign: "center" };
 const continueText = { ...baseText, fontSize: 14, color: "#892cdc" };
 export default StyleSheet.create({
+  baseText,
+  smallText,
+  continueText,
   screen: {
     padding: "10%",
     alignItems: "center",
@@ -12,8 +15,6 @@ export default StyleSheet.create({
     backgroundColor: "black",
   },
   bigText: { ...baseText, fontSize: 30 },
-  smallText,
-  continueText,
   errorText: { ...continueText, textAlign: "center", color: "red", padding: 10 },
   inputField: {
     ...smallText,
